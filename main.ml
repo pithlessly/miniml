70841e6d8874311057d1c72f157e254f8a32f458
pithlessly <79002325+pithlessly@users.noreply.github.com>
2023-03-20 11:26:12 -0700

    add lexer able to lex itself

---
+++ main.ml
@@ -0,0 +1,119 @@
+type token =
+  | OpenParen
+  | CloseParen
+  | OpenBracket
+  | CloseBracket
+  | Dot
+  | Colon
+  | Comma
+  | Semicolon
+  | Equal
+  | Pipe
+  | KType | KOf
+  | KLet | KRec | KIn
+  | KIf | KThen | KElse
+  | KMatch | KWith
+  | KFun
+  | KUnder
+  | IdentLower of string
+  | IdentUpper of string
+  | IdentSymbol of string
+  | IdentQuote of string
+  | CharLit of char
+  | IntLit of int
+  | StrLit of string
+
+let lex str =
+  (* character properties *)
+  let lower c = ('a' <= c && c <= 'z') || c = '_' in
+  let upper c = 'A' <= c && c <= 'Z' in
+  let numer c = '0' <= c && c <= '9' in
+  let ident c = upper c || lower c || numer c in
+  let symbolic c = match c with | '&' | '*' | '+' | '-' | '.' | ':'
+                                | '<' | '>' | '=' | '^' | '|' -> true
+                                | _ -> false
+  in
+  (* main logic *)
+  let length = String.length str in
+  let char i = if i < length then Some (String.get str i) else None in
+  let rec go i tokens =
+    let adv t = go (i + 1) (t :: tokens) in
+    let take_range d (included : char -> bool) (mk_tok : string -> token) =
+      let rec scan d = match char (i + d) with
+                       | Some c -> if included c then scan (d + 1) else d
+                       | None -> d
+      in
+      let d = scan d in go (i + d) (mk_tok (String.sub str i d) :: tokens)
+    in
+    match char i with
+    | Some '\n'
+    | Some ' ' -> go (i + 1) tokens
+    | Some '"' ->
+      let rec scan i parts escaped =
+        match (char i, escaped) with
+        | (None, _) -> Error "expected trailing quote for string literal, got EOF"
+        | (Some '"',  false) -> Ok (i + 1, StrLit (String.concat "" (List.rev parts)))
+        | (Some '\\', false) -> scan (i + 1) parts true
+        | (Some c,    false) -> scan (i + 1) (String.make 1 c :: parts) false
+        | (Some '"',  true)  -> scan (i + 1) ("\"" :: parts) false
+        | (Some '\\', true)  -> scan (i + 1) ("\\" :: parts) false
+        | (Some _,    true)  -> Error "invalid escape sequence in string literal"
+      in
+      (match scan (i + 1) [] false with
+       | Error e -> Error e
+       | Ok (i, tok) -> go i (tok :: tokens))
+    | Some '\'' ->
+      let char_done c i =
+        match char i with
+        | None -> Error "expected trailing quote for character literal, got EOF"
+        | Some '\'' -> go (i + 1) (CharLit c :: tokens)
+        | Some _ -> if lower c then take_range 1 ident (fun s -> IdentQuote s)
+                    else Error "unterminated character literal"
+      in
+      (match char (i + 1) with
+       | Some '\\' ->
+         (match char (i + 2) with
+          | Some 'n' -> char_done '\n' (i + 3)
+          | Some '\\' -> char_done '\\' (i + 3)
+          | Some '\'' -> char_done '\'' (i + 3)
+          | _ -> Error "invalid escape sequence in character literal")
+       | Some c -> char_done c (i + 2)
+       | _ -> Error "malformed character literal")
+    | Some '(' -> adv OpenParen
+    | Some ')' -> adv CloseParen
+    | Some '[' -> adv OpenBracket
+    | Some ']' -> adv CloseBracket
+    | Some ',' -> adv Comma
+    | Some ';' -> adv Semicolon
+    | Some c ->
+      let mk_lower_ident s = match s with
+                             | "type" -> KType | "of" -> KOf
+                             | "let" -> KLet | "rec" -> KRec | "in" -> KType
+                             | "if" -> KIf | "then" -> KThen | "else" -> KElse
+                             | "match" -> KMatch | "with" -> KWith
+                             | "fun" -> KFun
+                             | "_" -> KUnder
+                             | _ -> IdentLower s
+      in
+      let mk_symbolic_ident s = match s with
+                                | "." -> Dot
+                                | ":" -> Colon
+                                | "=" -> Equal
+                                | "|" -> Pipe
+                                | _ -> IdentSymbol s
+      in
+      if upper c then take_range 0 ident (fun s -> IdentUpper s) else
+      if lower c then take_range 0 ident mk_lower_ident else
+      if numer c then take_range 0 numer (fun s -> IntLit (int_of_string s)) else
+      if symbolic c then take_range 0 symbolic mk_symbolic_ident else
+        Error ("unexpected character: " ^ String.make 1 c
+               ^ " at position: " ^ string_of_int i)
+    | None -> Ok (List.rev tokens)
+  in
+  go 0 []
+
+let tokens =
+  let f = In_channel.open_text "main.ml" in
+  let text = In_channel.input_all f in
+  In_channel.close f;
+  lex text
