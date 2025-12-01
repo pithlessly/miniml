94931fb6cd118d96d856660fc1520457936854da
pithlessly <79002325+pithlessly@users.noreply.github.com>
2025-11-30 16:21:41 -0800

    initial_ctx: don't prefix all our type names with `t_`

--- compiler/initial_ctx.ml
+++ compiler/initial_ctx.ml
@@ -30,7 +30,16 @@ type 'typ prelude_setup = {
 }
 
 let initial_ctx {
-  types = { t_func = (@->); t_tuple; t_unit; t_char; t_int; t_string; t_bool; t_list };
+  types = {
+    t_func = (@->);
+    t_tuple = tuple;
+    t_unit = unit;
+    t_char = char;
+    t_int = int;
+    t_string = string;
+    t_bool = bool;
+    t_list = list;
+  };
   new_qvar;
   top_level = { add; add_con; ty0; ty1; ty2; add_mod; _ };
 } : unit =
@@ -41,97 +50,97 @@ let initial_ctx {
   let b = new_qvar "b" () in
   let c = new_qvar "c" () in
   add "|>"  (a @-> (a @-> b) @-> b);
-  add "&&"  (t_bool @-> t_bool @-> t_bool);
-  add "||"  (t_bool @-> t_bool @-> t_bool);
-  add "not" (t_bool @-> t_bool);
-  add "+"   (t_int @-> t_int @-> t_int);
-  add "-"   (t_int @-> t_int @-> t_int);
-  add "*"   (t_int @-> t_int @-> t_int);
-  add "/"   (t_int @-> t_int @-> t_int);
-  add ">="  (t_int @-> t_int @-> t_bool);
-  add "<="  (t_int @-> t_int @-> t_bool);
-  add ">"   (t_int @-> t_int @-> t_bool);
-  add "<"   (t_int @-> t_int @-> t_bool);
+  add "&&"  (bool @-> bool @-> bool);
+  add "||"  (bool @-> bool @-> bool);
+  add "not" (bool @-> bool);
+  add "+"   (int @-> int @-> int);
+  add "-"   (int @-> int @-> int);
+  add "*"   (int @-> int @-> int);
+  add "/"   (int @-> int @-> int);
+  add ">="  (int @-> int @-> bool);
+  add "<="  (int @-> int @-> bool);
+  add ">"   (int @-> int @-> bool);
+  add "<"   (int @-> int @-> bool);
   (* TODO: figure out a better alternative to polymorphic physical equality *)
-  add "="   (a @-> a @-> t_bool);
-  add "<>"  (a @-> a @-> t_bool);
-  add "=="  (a @-> a @-> t_bool);
-  add "^"   (t_string @-> t_string @-> t_string);
-  add ";"   (t_unit @-> a @-> a);
-  add "min" (t_int @-> t_int @-> t_int);
-  add "fst" (t_tuple [a; b] @-> a);
-  add "snd" (t_tuple [a; b] @-> b);
-  add "int_of_string" (t_string @-> t_int);
-  add "string_of_int" (t_int @-> t_string);
-  add "int_of_char"   (t_char @-> t_int);
-  add "char_of_int"   (t_int @-> t_char);
-  add "print_endline" (t_string @-> t_unit);
-  add "prerr_endline" (t_string @-> t_unit);
-  add "invalid_arg"   (t_string @-> a);
-  add "exit"          (t_int @-> a);
-  add_con "true"  [] t_bool;
-  add_con "false" [] t_bool;
+  add "="   (a @-> a @-> bool);
+  add "<>"  (a @-> a @-> bool);
+  add "=="  (a @-> a @-> bool);
+  add "^"   (string @-> string @-> string);
+  add ";"   (unit @-> a @-> a);
+  add "min" (int @-> int @-> int);
+  add "fst" (tuple [a; b] @-> a);
+  add "snd" (tuple [a; b] @-> b);
+  add "int_of_string" (string @-> int);
+  add "string_of_int" (int @-> string);
+  add "int_of_char"   (char @-> int);
+  add "char_of_int"   (int @-> char);
+  add "print_endline" (string @-> unit);
+  add "prerr_endline" (string @-> unit);
+  add "invalid_arg"   (string @-> a);
+  add "exit"          (int @-> a);
+  add_con "true"  [] bool;
+  add_con "false" [] bool;
   (
-    let t_ref = ty1 "ref" in
-    add "ref"   (a @-> t_ref a);
-    add "deref" (t_ref a @-> a);
-    add ":="    (t_ref a @-> a @-> t_unit)
+    let ref = ty1 "ref" in
+    add "ref"   (a @-> ref a);
+    add "deref" (ref a @-> a);
+    add ":="    (ref a @-> a @-> unit)
     );
-  add_con "::" [a; t_list a] (t_list a);
+  add_con "::" [a; list a] (list a);
   (* TODO: remove this; have AST treat (::) in expr as a constructor *)
-  add     "::" (a @-> t_list a @-> t_list a);
-  add     "@"  (t_list a @-> t_list a @-> t_list a);
-  let t_option = ty1 "option" in
-  add_con "None" []  (t_option a);
-  add_con "Some" [a] (t_option a);
+  add     "::" (a @-> list a @-> list a);
+  add     "@"  (list a @-> list a @-> list a);
+  let option = ty1 "option" in
+  add_con "None" []  (option a);
+  add_con "Some" [a] (option a);
   add_mod "Option" (fun { add; _ } ->
-    add "map"    ((a @-> b) @-> (t_option a @-> t_option b));
-    add "unwrap" (t_option a @-> a);
-    add "bind"   (t_option a @-> (a @-> t_option b) @-> t_option b);
+    add "map"    ((a @-> b) @-> (option a @-> option b));
+    add "unwrap" (option a @-> a);
+    add "bind"   (option a @-> (a @-> option b) @-> option b);
     ()
   );
   add_mod "List" (fun { add; _ } ->
-    add "init"       (t_int @-> (t_int @-> a) @-> t_list a);
-    add "rev"        (t_list a @-> t_list a);
-    add "fold_left"  ((a @-> b @-> a) @-> (a @-> t_list b @-> a));
-    add "fold_right" ((b @-> a @-> a) @-> (t_list b @-> a @-> a));
-    add "map"        ((a @-> b) @-> (t_list a @-> t_list b));
-    add "map2"       ((a @-> b @-> c) @-> (t_list a @-> t_list b @-> t_list c));
-    add "mapi"       ((t_int @-> a @-> b) @-> (t_list a @-> t_list b));
-    add "for_all"    ((a @-> t_bool) @-> t_list a @-> t_bool);
-    add "filter"     ((a @-> t_bool) @-> (t_list a @-> t_list a));
-    add "find_opt"   ((a @-> t_bool) @-> (t_list a @-> t_option a));
-    add "iter"       ((a @-> t_unit) @-> (t_list a @-> t_unit));
-    add "length"     (t_list a @-> t_int);
-    add "concat"     (t_list (t_list a) @-> t_list a);
-    add "concat_map" ((a @-> t_list b) @-> (t_list a @-> t_list b));
+    add "init"       (int @-> (int @-> a) @-> list a);
+    add "rev"        (list a @-> list a);
+    add "fold_left"  ((a @-> b @-> a) @-> (a @-> list b @-> a));
+    add "fold_right" ((b @-> a @-> a) @-> (list b @-> a @-> a));
+    add "map"        ((a @-> b) @-> (list a @-> list b));
+    add "map2"       ((a @-> b @-> c) @-> (list a @-> list b @-> list c));
+    add "mapi"       ((int @-> a @-> b) @-> (list a @-> list b));
+    add "for_all"    ((a @-> bool) @-> list a @-> bool);
+    add "filter"     ((a @-> bool) @-> (list a @-> list a));
+    add "find_opt"   ((a @-> bool) @-> (list a @-> option a));
+    add "iter"       ((a @-> unit) @-> (list a @-> unit));
+    add "length"     (list a @-> int);
+    add "concat"     (list (list a) @-> list a);
+    add "concat_map" ((a @-> list b) @-> (list a @-> list b));
     ()
   );
-  let t_result = ty2 "result" in
-  add_con "Ok"    [a] (t_result a b);
-  add_con "Error" [b] (t_result a b);
+  let result = ty2 "result" in
+  add_con "Ok"    [a] (result a b);
+  add_con "Error" [b] (result a b);
   add_mod "Char" (fun { add; _ } ->
-    add ">=" (t_char @-> t_char @-> t_bool);
-    add "<=" (t_char @-> t_char @-> t_bool);
-    add ">"  (t_char @-> t_char @-> t_bool);
-    add "<"  (t_char @-> t_char @-> t_bool);
+    add ">=" (char @-> char @-> bool);
+    add "<=" (char @-> char @-> bool);
+    add ">"  (char @-> char @-> bool);
+    add "<"  (char @-> char @-> bool);
     ()
   );
   add_mod "String" (fun { add; _ } ->
-    add "length"  (t_string @-> t_int);
-    add "get"     (t_string @-> t_int @-> t_char);
-    add "sub"     (t_string @-> t_int @-> t_int @-> t_string);
-    add "concat"  (t_string @-> t_list t_string @-> t_string);
-    add "make"    (t_int @-> t_char @-> t_string);
-    add "for_all" ((t_char @-> t_bool) @-> (t_string @-> t_bool));
-    add "filter"  ((t_char @-> t_bool) @-> (t_string @-> t_string));
-    add "<"       (t_string @-> t_string @-> t_bool);
-    add ">"       (t_string @-> t_string @-> t_bool);
+    add "length"  (string @-> int);
+    add "get"     (string @-> int @-> char);
+    add "sub"     (string @-> int @-> int @-> string);
+    add "concat"  (string @-> list string @-> string);
+    add "make"    (int @-> char @-> string);
+    add "for_all" ((char @-> bool) @-> (string @-> bool));
+    add "filter"  ((char @-> bool) @-> (string @-> string));
+    add "<"       (string @-> string @-> bool);
+    add ">"       (string @-> string @-> bool);
     ()
   );
-  let t_void = ty0 "void" in
+  let void = ty0 "void" in
   add_mod "Void" (fun { add; _ } ->
-    add "absurd" (t_void @-> a);
+    add "absurd" (void @-> a);
     ()
   );
   add_mod "Fun" (fun { add; _ } ->
@@ -140,45 +149,45 @@ let initial_ctx {
     ()
   );
   (
-    let t_in_channel = ty0 "in_channel" in
+    let in_channel = ty0 "in_channel" in
     add_mod "In_channel" (fun { add; _ } ->
-      add "open_text" (t_string @-> t_in_channel);
-      add "input_all" (t_in_channel @-> t_string);
-      add "close"     (t_in_channel @-> t_unit);
+      add "open_text" (string @-> in_channel);
+      add "input_all" (in_channel @-> string);
+      add "close"     (in_channel @-> unit);
       ()
     )
   );
   add_mod "StringMap" (fun { add; add_con; ty0; ty1; _ } ->
     let t = ty1 "t" in
     add "empty"     (t a);
-    add "singleton" (t_string @-> a @-> t a);
-    add "lookup"    (t_string @-> t a @-> t_option a);
-    add "eql"       ((a @-> a @-> t_bool) @-> (t a @-> t a @-> t_bool));
-    add "insert"    (t_string @-> a @-> t a @-> t_option (t a));
-    add "map"       ((t_string @-> a @-> b) @-> (t a @-> t b));
-    add "fold"      ((a @-> t_string @-> b @-> a) @-> (a @-> t b @-> a));
-    let t_dup_err = ty0 "dup_err" in
-    add_con "DupErr" [t_string] t_dup_err;
-    add "disjoint_union" (t a @-> t a @-> t_result (t a) t_dup_err);
+    add "singleton" (string @-> a @-> t a);
+    add "lookup"    (string @-> t a @-> option a);
+    add "eql"       ((a @-> a @-> bool) @-> (t a @-> t a @-> bool));
+    add "insert"    (string @-> a @-> t a @-> option (t a));
+    add "map"       ((string @-> a @-> b) @-> (t a @-> t b));
+    add "fold"      ((a @-> string @-> b @-> a) @-> (a @-> t b @-> a));
+    let dup_err = ty0 "dup_err" in
+    add_con "DupErr" [string] dup_err;
+    add "disjoint_union" (t a @-> t a @-> result (t a) dup_err);
     ()
   );
   add_mod "IntMap" (fun { add; ty1; _ } ->
     let t = ty1 "t" in
     add "empty"    (t a);
-    add "is_empty" (t a @-> t_bool);
-    add "lookup"   (t_int @-> t a @-> t_option a);
-    add "insert"   (t_int @-> a @-> t a @-> t a);
-    add "fold"     ((a @-> t_int @-> b @-> a) @-> (a @-> t b @-> a));
+    add "is_empty" (t a @-> bool);
+    add "lookup"   (int @-> t a @-> option a);
+    add "insert"   (int @-> a @-> t a @-> t a);
+    add "fold"     ((a @-> int @-> b @-> a) @-> (a @-> t b @-> a));
     add "union"    (t a @-> (t a @-> t a));
-    add "iter"     ((t_int @-> a @-> t_unit) @-> t a @-> t_unit);
-    add "filter"   ((t_int @-> a @-> t_bool) @-> t a @-> t a);
+    add "iter"     ((int @-> a @-> unit) @-> t a @-> unit);
+    add "filter"   ((int @-> a @-> bool) @-> t a @-> t a);
     ()
   );
   add_mod "Miniml" (fun { add; _ } ->
-    add "log_level" t_int;
-    add "debug" ((t_unit @-> t_string) @-> t_unit);
-    add "trace" ((t_unit @-> t_string) @-> t_unit);
-    add "argv"  (t_unit @-> t_list t_string);
+    add "log_level" int;
+    add "debug" ((unit @-> string) @-> unit);
+    add "trace" ((unit @-> string) @-> unit);
+    add "argv"  (unit @-> list string);
     ()
   );
   ()
